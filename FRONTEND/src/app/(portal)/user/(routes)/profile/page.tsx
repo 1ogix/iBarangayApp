5b79a30473e4dac4aa8efa023cdaddfb4250601d
1ogix <rubialan23@gmail.com>
2025-10-09 15:17:47 +0800

    fix vercel build

--- FRONTEND/src/app/(portal)/user/(routes)/profile/page.tsx
+++ FRONTEND/src/app/(portal)/user/(routes)/profile/page.tsx
@@ -1,11 +1,9 @@
-import { cookies } from "next/headers";
 import { PageHeader } from "@/components/layouts/page-header";
 
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+  const supabase = createClient();
 
   const {
     data: { user },
